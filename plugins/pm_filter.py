d305cdfdb4da61ae87db280fc388b9ea26ee0849
Zendaya <100590136+Botowner@users.noreply.github.com>
2022-06-26 08:14:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -90,7 +90,6 @@ async def next_page(bot, query):
     else:
         off_set = offset - 10
     if n_offset == 0:
-        )
         btn.append(
             [InlineKeyboardButton("⏪ BACK", callback_data=f"next_{req}_{key}_{off_set}"),
              InlineKeyboardButton(f"❄️Pages {round(int(offset) / 10) + 1} / {round(total / 10)}❄️",
