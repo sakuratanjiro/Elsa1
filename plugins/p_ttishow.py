0dbf6ff14f7c3d16f55dd1d7ebcaa3b7e5c5ce1b
Zendaya <100590136+Botowner@users.noreply.github.com>
2022-07-02 14:54:03 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -61,7 +61,7 @@ async def save_group(bot, message):
                                                  caption=(MELCOW_ENG.format(u.mention, message.chat.title)),
                                                  reply_markup=InlineKeyboardMarkup(
                                                                          [[
-                                                                           InlineKeyboardButton("🔖 ʀᴇᴀᴅ ɢʀᴏᴜᴘ ʀᴜʟᴇs 🔖", url="https://t.me/ccomautofilter_bot?start=BATCH-BQADBQAD-AUAAjOl0FXVH5XDilZ5ZBYE")
+                                                                           InlineKeyboardButton("🔖 ʀᴇᴀᴅ ɢʀᴏᴜᴘ ʀᴜʟᴇs 🔖", url="https://t.me/ccomfileshare_bot?start=Z2V0LTE4NDI5NjE2MTMyOTUxMg")
                                                                            ],[
                                                                            InlineKeyboardButton("🎭 ᴄʜᴀɴɴᴇʟ 🎭", url="https://t.me/cinemala_com1")
                                                                          
